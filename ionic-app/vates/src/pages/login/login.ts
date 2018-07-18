be206f98326492446ae17ff5ee0b0b033e2f8a7a
Gokul Krishna <gkgokul10@gmail.com>
2018-07-18 19:04:05 +0530

    Initial Commit: Template code

---
+++ ionic-app/vates/src/pages/login/login.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+  username: string;
+  password: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.username = '';
+    this.password = '';
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+  authenticate() {
+    console.log('Entered authenticate');
+    console.log('USERNAME: ' + this.username + ', PASSWORD: ' + this.password)
+    if (this.username == "hello" && this.password == "hello") {
+      console.log('SUCCESS');
+      this.navCtrl.setRoot('WelcomePage');
+    }
+  }
+
+}
